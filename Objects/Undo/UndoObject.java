ee09621d9c6ef6cb3bcb2a668a16dc4a7ef85702
Emmett Grebe <emmettgrebe@gmail.com>
2026-04-06 11:16:23 -0400

    Undo progress

--- Objects/Undo/UndoObject.java
+++ Objects/Undo/UndoObject.java
@@ -1,5 +1,26 @@
 package Objects.Undo;
 
+import java.nio.file.Path;
+import java.util.*;
+
+/**
+ * Object for storing undo data. Stores previous locations of files.
+ * 
+ * @author Emmett Grebe
+ * @version 4-6-2026
+ */
 public class UndoObject {
-    
+    private Map<String, String> previousNames;
+    private Map<String, Path> previousPaths;
+
+    public UndoObject() {
+        previousNames = new HashMap<String,String>();
+        previousPaths = new HashMap<String,Path>();
+    }
+
+
+
+    public Path getPreviousPath(String fileName) {
+
+    }
 }
--- Objects/Undo/UndoReader.java
+++ Objects/Undo/UndoReader.java
@@ -1,5 +1,11 @@
 package Objects.Undo;
 
+/**
+ * Object for storing undo data. Stores previous locations of files.
+ * 
+ * @author Emmett Grebe
+ * @version 4-6-2026
+ */
 public class UndoReader {
     
 }
